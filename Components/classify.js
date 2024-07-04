723bfd981cd6668cd4fa50d3194d45a57183d567
musharrafhamraz <musharrafhamraz02@gmail.com>
2024-07-04 12:33:32 +0500

    Trying to build.

--- Components/classify.js
+++ Components/classify.js
@@ -1,111 +1,330 @@
-import React, { useEffect, useState, useRef } from 'react';
-import { Text, View, Button, Image, Alert, Linking } from 'react-native';
-import { Camera, useCameraDevice } from 'react-native-vision-camera';
+// import React, { useState, useEffect } from 'react';
+// import { StyleSheet, Text, View, Image, TouchableOpacity, Platform, Dimensions, ImageBackground, StatusBar } from 'react-native';
+// import { launchCameraAsync, launchImageLibraryAsync } from 'expo-image-picker';
+// import { Ionicons } from '@expo/vector-icons';
+// import axios from 'axios';
 
-const CameraScreen = () => {
-  const [cameraPermission, setCameraPermission] = useState(null); // Initial state null to differentiate between loading state
-  const device = useCameraDevice('back'); // Set the initial camera device
-  const camera = useRef(null);
-  const [capturedPhoto, setCapturedPhoto] = useState(null);
-  const [showPreview, setShowPreview] = useState(false);
+// const { height, width } = Dimensions.get('window');
 
-  const checkCameraPermission = async () => {
-    const status = await Camera.getCameraPermissionStatus();
-    if (status === 'authorized' || status === 'limited') {
-      setCameraPermission(true);
-    } else if (status === 'not-determined') {
-      const permission = await Camera.requestCameraPermission();
-      setCameraPermission(permission === 'authorized' || permission === 'limited');
-    } else {
-      setCameraPermission(false);
+// const ImageClassifier = () => {
+//   const [result, setResult] = useState('');
+//   const [label, setLabel] = useState('');
+// //   const [image, setImage] = useState(require('./applescab.jpg')); // Replace with your default image
+
+//   useEffect(() => {
+//     StatusBar.setBarStyle('light-content');
+//   }, []);
+
+//   const handleCamera = async () => {
+//     let result = await launchCameraAsync({
+//       mediaTypes: 'Images',
+//       allowsEditing: true,
+//       aspect: [1, 1],
+//       quality: 0.5,
+//     });
+
+  
+//     if (!result.canceled) {
+//       setResult('');
+//       setLabel('Predicting...');
+//       getPrediction(result);
+//     }
+//   };
+  
+//   const handleImageLibrary = async () => {
+//     let result = await launchImageLibraryAsync({
+//       mediaTypes: 'Images',
+//       allowsEditing: true,
+//       aspect: [1, 1],
+//       quality: 0.5,
+//     });
+  
+//     if (!result.canceled) {
+//       setResult('');
+//       setLabel('Predicting...');
+//       getPrediction(result);
+//     }
+//   };
+
+//   const getPrediction = async (result) => {
+//     try {
+//       const uriParts = result.assets[0].uri.split('/');
+//       const fileName = uriParts[uriParts.length - 1];
+
+//       const formData = new FormData();
+//       console.log(formData)
+//       formData.append('file', {
+//         uri: result.assets[0].uri,
+//         type: 'image/jpeg',
+//         name: fileName,
+//       });
+  
+//       const response = await axios.post('https://cnn-model-api-deployment-ac2b40fcf26d.herokuapp.com/predict', formData, {
+//         headers: {
+//           'accept': 'application/json',
+//           'Content-Type': 'multipart/form-data',
+//           'access-control-allow-credentials': true 
+//         },
+//         timeout: 10000,
+//       });
+  
+//       const { data } = response;
+//       console.log("data", {data});
+  
+//       if (data && data.class) {
+//         setLabel(data.class);
+//         setResult(data.confidence);
+//       } else {
+//         setLabel('Failed to predict');
+//       }
+//     } catch (error) {
+//       // Handle errors
+//       if (error.response) {
+//         // Server responded with a status code outside of 2xx
+//         console.error('Server responded with error:', error.response.data);
+//       } else if (error.request) {
+//         // The request was made but no response was received
+//         console.error('No response received:', error.request);
+//       } else {
+//         // Other errors
+//         console.error('Error:', error.message);
+//       }
+//     }
+//   };
+  
+
+//   return (
+//     <View style={styles.container}>
+//       <ImageBackground source={{ uri: 'background' }} style={styles.backgroundImage} />
+//       <Text style={styles.title}>Potato Disease Prediction App</Text>
+//   {/* {image && <Image source={{ uri: image }} style={styles.image} />} */}
+//       <View style={styles.resultContainer}>
+//         <Text style={styles.resultText}>Label: {label}</Text>
+//         <Text style={styles.resultText}>Confidence: {parseFloat(result).toFixed(2) + '%'}</Text>
+//       </View>
+//       <View style={styles.buttonContainer}>
+//         <TouchableOpacity style={styles.iconButton} onPress={handleCamera}>
+//           <Ionicons name="camera" size={24} color="white" />
+//         </TouchableOpacity>
+//         <TouchableOpacity style={styles.iconButton} onPress={handleImageLibrary}>
+//           <Ionicons name="images" size={24} color="white" />
+//         </TouchableOpacity>
+//       </View>
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'space-between',
+//     paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+//   },
+//   backgroundImage: {
+//     position: 'absolute',
+//     width: '100%',
+//     height: '100%',
+//     zIndex: -1,
+//   },
+//   title: {
+//     fontSize: 30,
+//     fontWeight: 'bold',
+//     color: 'black',
+//     textAlign: 'center',
+//     marginTop: 40,
+//   },
+//   image: {
+//     width: width - 40,
+//     height: width - 40,
+//     borderRadius: 10,
+//     marginBottom: 20,
+//   },
+//   resultContainer: {
+//     backgroundColor: 'rgba(255, 255, 255, 0.8)',
+//     padding: 10,
+//     borderRadius: 10,
+//     marginBottom: 20,
+//   },
+//   resultText: {
+//     fontSize: 18,
+//     fontWeight: 'bold',
+//     marginBottom: 5,
+//   },
+//   buttonContainer: {
+//     flexDirection: 'row',
+//     justifyContent: 'space-around',
+//     width: '100%',
+//     marginBottom: 20,
+//   },
+//   iconButton: {
+//     backgroundColor: '#007bff',
+//     padding: 20,
+//     borderRadius: 5,
+//   },
+// });
+
+// export default ImageClassifier;
+
+import React, { useState, useEffect, useRef } from 'react';
+import { Text, View, StyleSheet, TouchableOpacity, ActivityIndicator } from 'react-native';
+import { Camera } from 'expo-camera';
+import { Ionicons } from '@expo/vector-icons';
+import axios from 'axios';
+import { useNavigation } from '@react-navigation/native';
+import * as ImageManipulator from 'expo-image-manipulator';
+
+const ImageClassifier = () => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [loading, setLoading] = useState(false);
+  const cameraRef = useRef(null);
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const takePicture = async () => {
+    if (cameraRef.current) {
+      let photo = await cameraRef.current.takePictureAsync();
+      setLoading(true);
+      compressImage(photo);
     }
   };
 
-  useEffect(() => {
-    checkCameraPermission();
-  }, []);
-
-  if (cameraPermission === null) {
-    return <Text>Checking camera permission...</Text>;
-  } else if (!cameraPermission) {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Camera permission not granted.</Text>
-        <Button
-          title="Open Settings"
-          onPress={() => Linking.openSettings()}
-        />
-      </View>
-    );
-  }
-
-  if (!device) {
-    return <Text>No camera device available</Text>;
-  }
-
-  const takePhoto = async () => {
+  const compressImage = async (photo) => {
     try {
-      if (!camera.current) {
-        console.error('Camera reference not available.', camera);
+      const compressedPhoto = await ImageManipulator.manipulateAsync(
+        photo.uri,
+        [{ resize: { width: 800 } }], // You can adjust the width as needed
+        { compress: 0.7, format: ImageManipulator.SaveFormat.JPEG }
+      );
+      getPrediction(compressedPhoto);
+    } catch (error) {
+      console.error('Error compressing image:', error);
+      setLoading(false);
+    }
+  };
+
+  const getPrediction = async (photo) => {
+    try {
+      if (!photo || !photo.uri) {
+        console.error('Invalid image data:', photo);
+        setLoading(false);
         return;
       }
 
-      const photo = await camera.current.takePhoto();
-      console.log(photo);
+      const formData = new FormData();
+      formData.append('file', {
+        uri: photo.uri,
+        type: 'image/jpeg',
+        name: 'photo.jpg',
+      });
 
-      if (photo) {
-        setCapturedPhoto(`file://${photo.path}`);
-        setShowPreview(true);
+      const response = await axios.post(
+        'https://cnn-model-api-deployment-ac2b40fcf26d.herokuapp.com/predict',
+        formData,
+        {
+          headers: {
+            accept: 'application/json',
+            'Content-Type': 'multipart/form-data',
+            'access-control-allow-credentials': true,
+          },
+        }
+      );
+
+      const { data } = response;
+
+      if (data && data.class) {
+        
+        navigation.navigate('treatment', { prediction: data.class, imageUri: photo.uri });
       } else {
-        console.error('Photo captured is undefined or empty.');
+        console.error('Failed to predict');
       }
     } catch (error) {
-      console.error('Error capturing photo:', error);
+      if (error.response) {
+        console.error('Error response:', error.response.data);
+      } else if (error.request) {
+        console.error('Error request:', error.request);
+      } else {
+        console.error('Error:', error.message);
+      }
+    } finally {
+      setLoading(false);
     }
   };
 
-  const confirmPhoto = () => {
-    console.log('Photo confirmed:', capturedPhoto);
-    setShowPreview(false); // Hide the preview after confirmation
-  };
-
-  const retakePhoto = () => {
-    setCapturedPhoto(null); // Clear the captured photo
-    setShowPreview(false); // Hide the preview
-  };
-
-  const onCameraReady = (ref) => {
-    camera.current = ref; // Reference to the Camera component (e.g., obtained from ref prop)
-  };
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
 
   return (
-    <View style={{ flex: 1 }}>
-      <Camera
-        style={{ flex: 1 }}
-        device={device}
-        isActive={true}
-        ref={(ref) => onCameraReady(ref)}
-        photo={true}
-        video={true}
-      />
-      {showPreview && capturedPhoto ? (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <Image
-            source={{ uri: capturedPhoto }} // Assuming the photo is a valid URI
-            style={{ width: 300, height: 300, marginBottom: 20 }}
-          />
-          <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-            <Button title="Retake" onPress={retakePhoto} />
-            <Button title="Confirm" onPress={confirmPhoto} />
-          </View>
+    <View style={styles.container}>
+      <Text style={styles.titleText}>Your Crop Doctor</Text>
+      <View style={styles.cameraWrapper}>
+        <Camera style={styles.camera} type={type} ref={cameraRef}>
+          <View style={{ flex: 1 }}></View>
+        </Camera>
+      </View>
+      <TouchableOpacity onPress={takePicture} disabled={loading}>
+        <View style={[styles.buttonContainer, loading && styles.buttonDisabled]}>
+          {loading ? (
+            <ActivityIndicator size="large" color="white" />
+          ) : (
+            <Ionicons name="camera" size={50} color="white" />
+          )}
         </View>
-      ) : (
-        <View style={{ flexDirection: 'row', justifyContent: 'space-evenly' }}>
-          <Button title="Take Photo" onPress={takePhoto} />
-        </View>
-      )}
+      </TouchableOpacity>
+      <Text style={styles.text}>Be aware of mistakes. Check important info</Text>
     </View>
   );
 };
 
-export default CameraScreen;
+export default ImageClassifier;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cameraWrapper: {
+    height: '60%',
+    width: '85%',
+    marginBottom: 12,
+    overflow: 'hidden',
+    borderRadius: 50,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    height: 80,
+    width: 80,
+    backgroundColor: 'green',
+    borderRadius: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    bottom: -30,
+  },
+  buttonDisabled: {
+    backgroundColor: 'gray',
+  },
+  text: {
+    bottom: -60,
+    opacity: 0.5,
+  },
+  titleText: {
+    fontSize: 30,
+    fontWeight: '500',
+    top: -30,
+    opacity: 0.7,
+  },
+});
